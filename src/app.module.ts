af7e07fbe96991433afec8f383b0ae6aa6b3933e
Naim <naim.bouallagui.8@gmail.com>
2020-02-23 13:13:18 +0100

    adding project module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectModule } from './modules/project/project.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ ProjectModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
