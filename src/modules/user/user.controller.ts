8cc6e76e0a02c3395f35e400cc6ae7c996cc3ba9
Naim <naim.bouallagui.8@gmail.com>
2020-03-10 12:57:08 +0100

    fix addclient api

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,9 +5,5 @@ import { UserService } from './user.service';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Post('addclient')
-  async addUser(@Body() obj: User): Promise<User> {
-    if ((obj.role = 'client')) return await this.userService.insertUser(obj);
-    // return  await this.investorService.investorClient(obj);
-  }
+
 }
