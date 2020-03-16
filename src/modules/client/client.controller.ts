f32a31e39cc4a792fec2ddac1f7398cbc35da382
Naim <naim.bouallagui.8@gmail.com>
2020-03-16 11:00:46 +0100

    fix interceptor

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseInterceptors } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { Client } from './client.interface';
-
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
+@UseInterceptors(LoggingInterceptor)
 @Controller('client')
 export class ClientController {
     constructor(private readonly clientService: ClientService) { }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors } from '@nestjs/common';
 import { User } from './user.interface';
 import { UserService } from './user.service';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 
 @Controller('user')
+@UseInterceptors(LoggingInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
