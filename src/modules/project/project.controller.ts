dfa96b5b6bbe53d9d0376e6c7dc25982a08070a8
Naim <naim.bouallagui.8@gmail.com>
2020-03-31 14:14:01 +0200

    add auth guard

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { Project } from './project.interface';
+import { AuthGuard } from '../passport/auth.guard';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 
 @Controller('project')
+@UseInterceptors(LoggingInterceptor)
 export class ProjectController {
   constructor(private readonly ProjectService: ProjectService) {}
+  
+  @UseGuards(AuthGuard('jwt', ['client']))
   @Post('addproject')
   async addProject(@Body() obj: Project): Promise<Project> {
     return await this.ProjectService.insertProject(obj);
