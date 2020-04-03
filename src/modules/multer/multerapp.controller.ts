67fc82e7cc23903f31cbf8e7edc665c6c9aa9d04
Naim <naim.bouallagui.8@gmail.com>
2020-04-03 16:48:41 +0200

    fix get uploaded file api

--- src/modules/multer/multerapp.controller.ts
+++ src/modules/multer/multerapp.controller.ts
@@ -1,37 +1,43 @@
 import {
-    Controller,
-    Post,
-    UseInterceptors,
-    UploadedFiles,
-  } from '@nestjs/common';
-  import { FilesInterceptor } from '@nestjs/platform-express';
-  import { diskStorage } from 'multer';
-  import { editFileName, imageFileFilter } from './multer';
-  
-  @Controller()
-  export class MulterAppController {
-  
-    @Post('upload/multiple')
-    @UseInterceptors(
-      FilesInterceptor('files', 20, {
-        storage: diskStorage({
-          destination: './upload',
-          filename: editFileName,
-        }),
-        fileFilter: imageFileFilter,
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFiles,
+  Param,
+  Get,
+  Res,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { editFileName, imageFileFilter } from './multer';
+
+@Controller()
+export class MulterAppController {
+  @Post('upload/multiple')
+  @UseInterceptors(
+    FilesInterceptor('files', 20, {
+      storage: diskStorage({
+        destination: './upload',
+        filename: editFileName,
       }),
-    )
-    async uploadMultipleFiles(@UploadedFiles() files) {
-      const response = [];
-      files.forEach(file => {
-        const fileReponse = {
-          originalname: file.originalname,
-          filename: file.filename,
-          file: file
-        };
-        response.push(fileReponse);
-      });
-      return response;
-    }
+      fileFilter: imageFileFilter,
+    }),
+  )
+  async uploadMultipleFiles(@UploadedFiles() files) {
+    const response = [];
+    files.forEach(file => {
+      const fileReponse = {
+        originalname: file.originalname,
+        filename: file.filename,
+        file: file,
+      };
+      response.push(fileReponse);
+    });
+    return response;
   }
-  
+
+  @Get('upload/:filename')
+  async getFile(@Param('filename') filename: string, @Res() res) {
+    return res.sendFile(filename, { root: './upload' });
+  }
+}
